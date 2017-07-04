028b61a7e2d426a5c05c31a2858191da9224d612
Perrine GERNEZ <pgernez@student.42.fr>
2017-07-04 23:59:04 +0200

    reprise, debut print table

--- ft_print_table.c
+++ ft_print_table.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   ft_print_table.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgernez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/26 19:32:35 by pgernez           #+#    #+#             */
-/*   Updated: 2017/03/12 19:30:19 by pgernez          ###   ########.fr       */
+/*   Created: 2017/07/04 22:33:19 by pgernez           #+#    #+#             */
+/*   Updated: 2017/07/04 22:49:48 by pgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include <stdio.h>
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
+int	print_table(char**)
+{
 
-# define BUF_SIZE 100
+}
+int	main(void)
+{
+	char tab[10][15] = {"Hello", "mes", "petites", "écrevisses", "rouges."};
 
-void	ft_putstr(char *str);
-void	ft_putchar(char c);
-int		read_file(char **argv);
-
-/* Add prototypes */
-
-#endif
+	printf("La première chaine : %s", tab[0]);
+	return (0);
+}
